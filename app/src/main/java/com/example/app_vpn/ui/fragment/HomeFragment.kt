5d3634a1838bf23757d2656ef0c32c60893d4542
giakhuu <giakhuu18112004@gmail.com>
2024-05-31 13:16:33 +0700

    Đã gọi nhau một tiếng anh em, thì vài ba chuyện nhỏ này có là gì chứ!!

--- app/src/main/java/com/example/app_vpn/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/app_vpn/ui/fragment/HomeFragment.kt
@@ -342,16 +342,22 @@ class HomeFragment : Fragment() {
                 e.printStackTrace()
             }
 
-            try {
-                mService?.registerStatusCallback(mCallback)
-            } catch (e: RemoteException) {
-                Log.e("HomeFragment", "Failed to register VPN status callback: ${e.message}")
-                e.printStackTrace()
-            }
         }
 
         override fun onServiceDisconnected(className: ComponentName) {
             mService = null // Set mService to null when service is disconnected
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 7) {
+            try {
+                mService!!.registerStatusCallback(mCallback)
+            } catch (e: RemoteException) {
+                Log.d("mCallback","openvpn status callback failed: " + e.message)
+                e.printStackTrace()
+            }
         }
     }
 
